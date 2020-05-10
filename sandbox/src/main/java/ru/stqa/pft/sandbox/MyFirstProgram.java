1db8729ae05938fdb877f287927bb992ceb12fd6
yaninapavlyk <yana.yankee@gmail.com>
2020-05-10 17:34:06 +0300

    added funcs hello, area, area

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,9 +4,31 @@ package ru.stqa.pft.sandbox;
 //классовБ название класса должно совпадать с названием файла
 	public class MyFirstProgram {
 // внутри класса нужно создать функцию с названием мейн 
-		public static void main(String[] args) { 
-								//парметры из коммандной строки
-			System.out.println("Hello, world");
+		public static void main(String[] args) {
+			hello("world");
+			hello("Yana");
+//
+			double l = 5;
+			System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+//			System.out.println("2 + 2 =" + 2 + 2);
+//			System.out.println("2 + 2 =" + (2 + 2));
+
+			double a = 4;
+			double b = 6;
+
+			System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+		}
+
+		public static void hello(String sb) {
+			System.out.println("Hello, " + sb + "!");
+		}
+
+		public static double area(double len){
+			return len * len;
+		}
+		public static double area(double a, double b){
+			return a * b;
 		}
 		
 	}
