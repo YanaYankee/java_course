527cd4e6a88ebd9c2a384aea379ae56c5ee30f21
yaninapavlyk <yana.yankee@gmail.com>
2020-05-24 14:33:33 +0300

    Task 2: fix

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.sandbox;
 
-import static ru.stqa.pft.sandbox.Point.distance;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,14 +8,13 @@ public class Main {
 
         System.out.println(
                 "Distance between points p1 and p2 with coordinates (" + p1.x + ", " + p1.y +
-                        ") and (" + p2.x + ", " + p2.y + ") equals " + distance(p1, p2));
+                        ") and (" + p2.x + ", " + p2.y + ") equals " + p1.distance(p2));
 
         Point p3 = new Point(5, 6);
         Point p4 = new Point(3, 8);
 
         System.out.println(
                 "Distance between p3 and p4 with coordinates (" + p3.x + ", " + p3.y +
-                        ") and (" + p3.x + ", " + p3.y + ") equals " + distance(p3, p4));
+                        ") and (" + p3.x + ", " + p3.y + ") equals " + p3.distance(p4));
         }
-
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -11,8 +11,8 @@ public class Point {
         this.y = y;
     }
 
-    public static double distance(Point p1, Point p2) {
-        double d = Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    public double distance(Point p) {
+        double d = Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
         return d;
     }
 
